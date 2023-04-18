38819a19a8784ef16fe76023cb31485c6faeeb58
SeniorYoan <yoan@kipsoft.fr>
2023-04-18 18:47:04 +0200

    Change migration content

--- migrations/01-create-users-table.php
+++ migrations/01-create-users-table.php
@@ -1,19 +1,24 @@
 <?php
 
+require_once 'core/Database.php';
 require_once 'script/migrate.php';
 
-if (!tableExists('users')) {
+$pdo = Database::connect();
+
+if (!tableExists($pdo,'users')) {
     $query = "CREATE TABLE users (
         id INT(11) PRIMARY KEY AUTO_INCREMENT,
         email VARCHAR(255) NOT NULL,
         password VARCHAR(255) NOT NULL,
         created_at DATETIME NOT NULL,
         username VARCHAR(255) NOT NULL,
-        account_type ENUM('rédacteur', 'modérateur', 'admin') NOT NULL,
-        is_validated BOOLEAN NOT NULL,
-        is_banned BOOLEAN NOT NULL,
+        account_type ENUM('reader', 'modo', 'admin') DEFAULT 'reader',
+        is_validated BOOLEAN DEFAULT false,
+        is_banned BOOLEAN DEFAULT false,
         reason_for_ban TEXT DEFAULT NULL
     )";
+
+    $pdo->exec($query);
 }
 
 ?>
