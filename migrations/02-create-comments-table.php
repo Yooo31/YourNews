d2acb6c3a066d790e6e165794d606ca728afcc68
SeniorYoan <yoan@kipsoft.fr>
2023-04-18 18:46:24 +0200

    Create migration to comment article

---
+++ migrations/02-create-comments-table.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once 'core/Database.php';
+require_once 'script/migrate.php';
+
+$pdo = Database::connect();
+
+if (!tableExists($pdo,'comments')) {
+    $query = "CREATE TABLE comments (
+        id INT(11) PRIMARY KEY AUTO_INCREMENT,
+        user_id INT(11) NOT NULL,
+        article_id INT(11) NOT NULL,
+        content TEXT NOT NULL,
+        created_at DATETIME NOT NULL,
+        updated_at DATETIME DEFAULT NULL,
+        is_approved BOOLEAN DEFAULT false
+    )";
+
+    $pdo->exec($query);
+}
+
+?>
