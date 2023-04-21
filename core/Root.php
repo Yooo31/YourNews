6998fd862d2033fbd2edaacd6780504f6c5a0bd7
Yooo31 <mrts.yoan@gmail.com>
2023-04-21 16:41:16 +0200

    System of session started, and changment of rooting with add Controller

--- core/Root.php
+++ core/Root.php
@@ -2,32 +2,70 @@
 
 class Root {
   private $controller;
+  private $routes = [];
 
-  public function __construct() {
-    $url = $this->parseUrl();
+  public function __construct($url)
+  {
+    $this->controller = new Controller();
+    $this->parseUrl($url);
+  }
 
-    $controllerName = $url[0] ?? 'home';
-    $controllerClassName = ucfirst($controllerName) . 'Controller';
-    $controllerFileName = 'controller/' . $controllerClassName . '.php';
+  public function get($path, $controllerMethod)
+  {
+    $this->routes['GET'][$path] = $controllerMethod;
+  }
 
-    if (file_exists($controllerFileName)) {
-      require_once($controllerFileName);
-      $this->controller = new $controllerClassName();
-    } else {
-      // gestion d'erreur : afficher une page 404 par exemple
+  public function post($path, $controllerMethod)
+  {
+    $this->routes['POST'][$path] = $controllerMethod;
+  }
+
+  public function run()
+  {
+    $method = $_SERVER['REQUEST_METHOD'];
+    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+    $routeFound = false;
+
+    foreach ($this->routes[$method] as $route => $controllerMethod) {
+      // Si la route contient des paramètres, on construit une expression régulière pour les récupérer
+      if (preg_match('#^' . preg_replace('#/:([\w]+)#', '/(?<$1>[^/]+)', $route) . '$#', $path, $matches)) {
+        $routeFound = true;
+
+        $controllerMethod = explode('@', $controllerMethod);
+        $controllerName = $controllerMethod[0];
+        $methodName = $controllerMethod[1];
+
+        if (method_exists($controllerName, $methodName)) {
+          $controller = new $controllerName();
+          // On passe les paramètres de l'URL sous forme de tableau associatif à la méthode du contrôleur
+          $controller->{$methodName}($matches);
+        } else {
+          // gestion d'erreur : afficher une page 404 par exemple
+        }
+        break;
+      }
     }
 
-    $actionName = $url[1] ?? 'index';
-    if (method_exists($this->controller, $actionName)) {
-      $this->controller->{$actionName}();
-    } else {
+    if (!$routeFound) {
       // gestion d'erreur : afficher une page 404 par exemple
     }
   }
 
-  private function parseUrl() {
-    if (isset($_GET['url'])) {
-      return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+  private function parseUrl($url)
+  {
+    $url = rtrim($url, '/');
+    $url = filter_var($url, FILTER_SANITIZE_URL);
+    $url = explode('/', $url);
+
+    $controller = ucfirst($url[0]) . 'Controller';
+    $this->controller = new $controller();
+
+    $method = isset($url[1]) ? $url[1] : 'index';
+    if (method_exists($this->controller, $method)) {
+      $this->controller->{$method}();
+    } else {
+      // gestion d'erreur
     }
   }
 }
--- index.php
+++ index.php
@@ -2,7 +2,34 @@
 
 require_once 'core/Root.php';
 require_once 'core/Database.php';
+require_once 'core/Session.php';
+
+Session::start();
 
 $db = Database::connect();
 
-$router = new Root($_GET['url']);
+$root = new Root($_GET['url']);
+
+$root->get('/', 'HomeController@index');
+$root->get('/login', 'AuthController@loginForm');
+$root->post('/login', 'AuthController@login');
+$root->get('/register', 'AuthController@registerForm');
+$root->post('/register', 'AuthController@register');
+$root->get('/logout', 'AuthController@logout');
+$root->get('/post/create', 'PostController@create');
+$root->post('/post', 'PostController@store');
+$root->get('/post/:id/edit', 'PostController@edit');
+$root->post('/post/:id', 'PostController@update');
+$root->post('/post/:id/delete', 'PostController@delete');
+$root->post('/post/:id/approve', 'PostController@approve');
+$root->post('/post/:id/reject', 'PostController@reject');
+$root->post('/comment/:id', 'CommentController@store');
+$root->post('/comment/:id/delete', 'CommentController@delete');
+$root->post('/comment/:id/approve', 'CommentController@approve');
+$root->post('/comment/:id/reject', 'CommentController@reject');
+$root->get('/user/:id', 'UserController@show');
+$root->post('/user/:id/delete', 'UserController@delete');
+$root->post('/user/:id/promote', 'UserController@promote');
+$root->post('/user/:id/demote', 'UserController@demote');
+
+$root->run();
