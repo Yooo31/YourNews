1c7bfd071003e1829c3f625db960ac66b1d66a96
Yooo31 <mrts.yoan@gmail.com>
2023-05-05 19:01:39 +0200

    Add new navbar

---
+++ app/navbar.php
@@ -0,0 +1,30 @@
+<nav>
+  <div class="nav-left">
+    <div class="logo">
+      <h2>YN</h2>
+      <img src="" alt="">
+    </div>
+    <div class="toggle">
+      <i class="fa-solid fa-bars open"></i>
+      <i class="fa-solid fa-times close"></i>
+    </div>
+    <ul class="nav-items">
+      <li><a class="text-black" href="#">Accueil</a></li>
+      <li><a class="text-black" href="#">Blog</a></li>
+      <li><a class="text-black" href="#">Administration</a></li>
+    </ul>
+  </div>
+  <div class="nav-right">
+    <ul class="nav-items">
+      <li>
+        <button class="nav-btn nav-new bg-primary text-white">Nouveau</button>
+      </li>
+      <li>
+        <button class="nav-btn nav-connect text-black">
+          <i class="fa-regular fa-user"></i>
+          <i class="fa-solid fa-caret-down"></i>
+        </button>
+      </li>
+    </ul>
+  </div>
+</nav>
