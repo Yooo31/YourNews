7d5fd21782cadbd15fb653876c9fc752591e3520
Yooo31 <mrts.yoan@gmail.com>
2023-05-25 17:48:16 +0200

    Add system global to login and login view

--- app/controller/AuthController.php
+++ app/controller/AuthController.php
@@ -4,18 +4,21 @@ require_once 'core/Controller.php';
 
 class AuthController extends Controller {
   public function showLoginForm() {
-
+    $this->view('Auth/login');
   }
 
   public function login() {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-  }
+    $authModel = new Auth();
+    $user = $authModel->getUserByUsername($username);
 
-  public function showRegistrationForm() {
-
-  }
-
-  public function register() {
-
+    if (!$user || !password_verify($password, $user['password'])) {
+      // Mauvaise authentification, afficher un message d'erreur ou rediriger vers la page de connexion
+    } else {
+      // Authentification réussie, connecter l'utilisateur (par exemple, en créant une session) et rediriger vers une page sécurisée
+    }
   }
 }
+
---
+++ app/view/Auth/login.php
@@ -0,0 +1,36 @@
+<div class="form register-form covid-form">
+  <div class="box box-form">
+    <div class="box-form-head">
+      <h3 class="mb-2">Se connecter</h3>
+    </div>
+    <div class="box-form-content mt-2">
+      <form method="POST" action="">
+
+        <div class="form-group mb-4">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="far fa-building"></i></span>
+            </div>
+            <input type="text" class="form-control" name="name" id="name" placeholder="Pseudo">
+          </div>
+        </div>
+
+        <div class="form-group mb-4">
+          <div class="input-group">
+            <div class="input-group-prepend">
+              <span class="input-group-text"><i class="fas fa-key"></i></span>
+            </div>
+            <input type="password" class="form-control" name="password" id="password" placeholder="Mot de passe">
+          </div>
+        </div>
+
+        <div class="message"></div>
+
+        <div class="cta text-center mt-4 mb-1">
+          <button type="button" name="submit_register" id="registerButton" class="button button-medium button-orange">Se connecter</button>
+        </div>
+
+      </form>
+    </div>
+  </div>
+</div>
