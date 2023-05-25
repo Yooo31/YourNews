7bb522a96630ed08f640b02536c8c60b241a5d6d
Yooo31 <mrts.yoan@gmail.com>
2023-05-25 16:52:51 +0200

    Blog page with all posts

---
+++ app/view/Posts/_posts.php
@@ -0,0 +1,16 @@
+<?php foreach ($posts as $post) : ?>
+  <div class="col-4">
+    <div class="post-view mb-5">
+      <div class="h2">
+        <a href="#">
+          <div class="post-picture lazy" style="background-image: url(&quot;assets/img/jsp.png&quot;);"></div>
+          <div class="post-desc pl-4">
+            <div class="h3"><?php echo $post['title']; ?></div>
+            <p><?php echo $post['description']; ?></p>
+            <div class="date"><?php echo date_format(date_create($post['created_at']), "d F"); ?></div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </div>
+<?php endforeach; ?>
--- app/view/Posts/index.php
+++ app/view/Posts/index.php
@@ -1,1 +1,7 @@
-<h2>Test</h2>
+<div class="mt-5"></div>
+
+<div class="container mt-5 mb-5">
+  <div class="row">
+  <?php $posts ? require_once '_posts.php' : ''; ?>
+  </div>
+</div>
